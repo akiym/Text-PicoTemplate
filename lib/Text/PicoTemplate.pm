bc624d9b7eaf7ba23f5f2a84762e0b49897b3c83
Takumi Akiyama <t.akiym@gmail.com>
2013-02-13 23:18:08 +0900

    initial commit

---
+++ lib/Text/PicoTemplate.pm
@@ -0,0 +1,111 @@
+package Text::PicoTemplate;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.01';
+
+use parent qw/Exporter/;
+use Carp ();
+
+our @EXPORT = qw/render_string/;
+
+our $TAG_START = '<%';
+our $TAG_END   = '%>';
+
+sub render_string {
+    my ($string, $vars) = @_;
+    $vars = {} unless defined $vars;
+
+    unless (ref $vars eq 'HASH') {
+        Carp::croak('Template variables must be a HASH reference');
+    }
+
+    $string =~ s{$TAG_START \s* (\S+?) \s* $TAG_END}{
+        if (exists $vars->{$1}) {
+            $vars->{$1};
+        } else {
+            _error($string, $1, @-);
+        }
+    }egx;
+    return $string;
+}
+
+sub _error {
+    my ($string, $var, @offsets) = @_;
+    my $line_offset = 4;
+
+    my $after = substr $string, 0, $offsets[0];
+    my $line = 1;
+    $line++ while $after =~ /\r\n?|\n/g;
+
+    my $delim = '-' x 76;
+
+    my $report = "Undefined symbol: $var at line $line.\n";
+    my $template = _context($string, $line);
+    $report .= "$delim\n$template$delim\n";
+    die $report;
+}
+
+sub _context {
+    my ($string, $line) = @_;
+    my @lines = split /\r\n?|\n/, $string;
+    return join '', map {
+        0 < $_ && $_ <= @lines ? sprintf("%4d: %s\n", $_, $lines[$_ - 1]) : ''
+    } ($line - 2) .. ($line + 2);
+}
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Text::PicoTemplate - Pico template engine only render variables
+
+=head1 SYNOPSIS
+
+  use Text::PicoTemplate;
+
+  print render_string('<% local_part %>@example.com', {
+      local_part => 'foo',
+  });
+
+=head1 DESCRIPTION
+
+Text::PicoTemplate is pico template engine. No escape variables, no expressions.
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 FUNCTIONS
+
+=head2 render_string($string, \%vars)
+
+Renders a template string with given variables.
+
+  print render_string('hello, <% john %>!', {john => 'John'});
+  # => hello, John!
+
+=head1 FAQ
+
+=head2 How do I change the style of tags?
+
+  use Text::PicoTemplate;
+
+  $Text::PicoTemplate::TAG_START = '[%';
+  $Text::PicoTemplate::TAG_END   = '%]';
+
+=head1 AUTHOR
+
+Takumi Akiyama E<lt>akiym@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+L<Text::MicroTemplate>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/01_basic.t
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+use utf8;
+use Test::More;
+use Text::PicoTemplate;
+
+is render_string('foo'), 'foo';
+is render_string('Hello, <% stuff %>!', {stuff => 'world'}), 'Hello, world!';
+is render_string('<% greeting %>, <% person %>!', {
+    greeting => 'こんにちは',
+    person   => 'professor',
+}), 'こんにちは, professor!';
+is render_string('Hello, <% 地球 %>!', {'地球' => 'world'}), 'Hello, world!';
+is render_string('Hello, <%
+    stuff
+%>!', {stuff => 'world'}), 'Hello, world!';
+
+is render_string('<% %>'), '<% %>';
+is render_string('foo <% bar bar %>', {'bar bar' => 'baz'}), 'foo <% bar bar %>';
+
+{
+    local $@;
+    eval {
+        render_string('foo <% bar %>');
+    };
+    like $@, qr/Undefined symbol: bar at line 1\./;
+}
+{
+    local $@;
+    eval {
+        render_string(<<'...');
+ABC
+DEF
+GHI
+<% JKL %>
+MNO
+...
+    };
+    like $@, qr/Undefined symbol: JKL at line 4\./;
+}
+
+done_testing;
